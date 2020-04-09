e6fbdb1d5e1618d89b7130fc4e8b48a5bb43a6fc
yoonsy95 <yegyo95@gmail.com>
2020-04-10 01:59:44 +0900

    springDAO로 DB 연결

---
+++ springdao/src/lab/spring/test/DAOTest.java
@@ -0,0 +1,57 @@
+package lab.spring.test;
+
+import java.util.Iterator;
+import java.util.List;
+
+import lab.spring.user.User;
+import lab.spring.user.UserService;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DAOTest {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"user_dao.xml");
+		UserService service = (UserService) context.getBean("userService");
+		/*
+		service.login("admin", "a1234");
+		System.out.println();
+		User user = new User();
+		user.setUserid("s3");
+		user.setUsername("?„œ?š¸3");
+		user.setUserpwd("1234");
+		user.setEmail("seoul3@korea.or.kr");
+		user.setPhone("02-129");
+		System.out.println("insert rows = >" + service.addUser(user));
+		System.out.println();
+
+		List<User> lists = service.findUserList();
+		Iterator<User> iter = lists.iterator();
+		while (iter.hasNext()) {
+			User u = iter.next();
+			System.out.println(u);
+		}
+		*/
+		System.out.println(service.findUser("s3"));
+		User user = new User();
+		user.setUserid("s3");		
+		user.setEmail("s3@gmail.or.kr");
+		user.setPhone("02-129-1234");
+		System.out.println("update :s3 =>"+service.updateUser(user));
+		System.out.println(service.findUser("s3"));
+		System.out.println("delete :s3 =>"+service.removeUser("s3"));
+		
+	}
+
+}
+
+
+
+
+
+
+
+
---
+++ springdao/src/lab/spring/user/UserDAO.java
@@ -0,0 +1,14 @@
+package lab.spring.user;
+
+import java.util.List;
+
+public interface UserDAO {
+	public int addUser(User user);
+	public int updateUser(User user);
+	public int removeUser(String uid);
+	public User findUser(String uid);
+	public boolean login(String uid, String upwd);
+	public List<User> findUserList();
+	
+
+}
