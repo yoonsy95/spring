6d6e4644cf81e7b406df411e29b751246b109988
yoonsy95 <yegyo95@gmail.com>
2020-04-17 03:49:29 +0900

    HelloMVC

---
+++ springMVC/HelloMVC/src/main/java/lab/spring/hello/HelloController.java
@@ -0,0 +1,14 @@
+package lab.spring.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	@RequestMapping("hello.do")
+	public String hello(Model model) {
+		model.addAttribute("greeting","hello");
+		return "hello";
+	}
+}
